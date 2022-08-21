fbad06a00f8096297661a51979b91c2cfea5eee8
Matthew Borkowski <55469998+Sulima1@users.noreply.github.com>
2022-08-20 20:31:40 -0400

    Updates to backend

--- NewMethod/app.py
+++ NewMethod/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, Response
 from database.db import initialize_db
 from database.models import PropertyReviews, LandlordReviews, Properties, Landlords
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 app.config['MONGODB_SETTINGS'] = {
--- NewMethod/database/models.py
+++ NewMethod/database/models.py
@@ -4,14 +4,20 @@ class PropertyReviews(db.EmbeddedDocument):
     Title = db.StringField(required=True)
     Body = db.StringField(required=True)
 
+
+#LANDLORD PAGE COMMENT FORMAT Stars, Address, Title, Body
+
 class LandlordReviews(db.EmbeddedDocument):
     Stars = db.IntField(required=True, min_value=0, max_value=10)
     Address = db.StringField(required=True)
+    Title = db.StringField(required=True)
+    Body = db.StringField(required=True)
 
 class Properties(db.Document):
     meta = {"collection": "Properties"}
     Category = db.StringField(required=True)
     Address = db.StringField(required=True)
+    Campus = db.StringField(required=True)
     Images = db.ImageField(required=True, unique=True, size=None, thumbnail_size=None)
     Rooms = db.IntField(required=True, min_value=0)
     Bathrooms = db.IntField(required=True, min_value=0)
